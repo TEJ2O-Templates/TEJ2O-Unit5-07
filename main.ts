39bb26ea0936b50eddfee1bd5ea66a391e8f0c43
Patrick Coxall <mr.coxall@mths.ca>
2023-11-07 11:33:24 -0500

    linting fixes

--- main.ts
+++ main.ts
@@ -8,13 +8,13 @@
 // variables
 const servoNumber1 = robotbit.Servos.S1
 
-//setup
+// setup
 basic.showIcon(IconNames.Happy)
 
-input.onButtonPressed(Button.A, function() {
+input.onButtonPressed(Button.A, function () {
   robotbit.Servo(servoNumber1, 0)
   basic.clearScreen()
-  basic.showString("0")
+  basic.showString('0')
   basic.showIcon(IconNames.SmallSquare)
   basic.clearScreen()
   basic.showIcon(IconNames.Happy)
@@ -23,7 +23,7 @@ input.onButtonPressed(Button.A, function() {
 input.onButtonPressed(Button.B, function () {
   robotbit.Servo(servoNumber1, 180)
   basic.clearScreen()
-  basic.showString("180")
+  basic.showString('180')
   basic.showIcon(IconNames.SmallSquare)
   basic.clearScreen()
   basic.showIcon(IconNames.Happy)
