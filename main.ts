638b3dea9701b54d6bfdc7b6bf094b1112f9e73c
Patrick Coxall <mr.coxall@mths.ca>
2023-11-07 10:04:56 -0500

    typescript version

--- main.ts
+++ main.ts
@@ -2,7 +2,29 @@
  *
  * Created by: Mr. Coxall
  * Created on: Sep 2020
- * This program ...
+ * This program controls a servo.
 */
 
-basic.showString('Hello, World!')
+// variables
+let servoNumber1 = robotbit.Servos.S1
+
+//setup
+basic.showIcon(IconNames.Happy)
+
+input.onButtonPressed(Button.A, function() {
+  robotbit.Servo(servoNumber1, 0)
+  basic.clearScreen()
+  basic.showString("0")
+  basic.showIcon(IconNames.SmallSquare)
+  basic.clearScreen()
+  basic.showIcon(IconNames.Happy)
+})
+
+input.onButtonPressed(Button.B, function () {
+  robotbit.Servo(servoNumber1, 180)
+  basic.clearScreen()
+  basic.showString("180")
+  basic.showIcon(IconNames.SmallSquare)
+  basic.clearScreen()
+  basic.showIcon(IconNames.Happy)
+})
